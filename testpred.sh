9f28bba00a6fe6ec155e0ba719b3c8715fd73434
Martin <martin.ng@live.com>
2024-05-31 18:42:13 +0800

    Add test scripts

---
+++ testpred.sh
@@ -0,0 +1,3 @@
+curl -L 'https://8h09twey46.execute-api.ap-southeast-1.amazonaws.com/dev' \
+-H 'Content-Type: application/json' \
+-d '{"date": "2026-01-01"}'
---
+++ teststatus.sh
@@ -0,0 +1,1 @@
+curl -L 'https://8h09twey46.execute-api.ap-southeast-1.amazonaws.com/dev/'
